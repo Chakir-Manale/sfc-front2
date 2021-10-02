c2f6128a39daaabaaf939834ef5a019af82ed791
Chakir-manale <50245117+Chakir-Manale@users.noreply.github.com>
2021-10-02 23:45:02 +0100

    blog

--- src/components/pages/blogs/grid-2cols.jsx
+++ src/components/pages/blogs/grid-2cols.jsx
@@ -1,13 +1,23 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import isotope from 'isotope-layout';
 import imagesLoaded from 'imagesloaded';
-import { Helmet } from 'react-helmet';
-import { isotopeLoad } from '../../../utils';
+import {Helmet} from 'react-helmet';
+import {isotopeLoad} from '../../../utils';
+import {Link} from "react-router-dom";
+import {connect, useDispatch} from "react-redux";
+import {getArticles} from "../../../actions/blogActions";
 
-export default function Grid2Cols() {
-    useEffect( () => {
-        isotopeLoad( isotope, imagesLoaded, '.entry-container', '.entry-item', '.entry-filter' );
-    } )
+function Grid2Cols(props) {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        isotopeLoad(isotope, imagesLoaded, '.entry-container', '.entry-item', '.entry-filter');
+    });
+
+    let articles = props.articles;
+    useEffect(() => {
+        getArticles();
+    }, [dispatch]);
+
 
     return (
         <>
@@ -17,166 +27,58 @@ export default function Grid2Cols() {
 
             <h1 className="d-none">SFC - Blog</h1>
 
-        <header className="header-bottom-4">
-            <img src="assets/images/banner/banner4.jpg"/>
-            <div className="header-bottom-4__bg">
-                <div className="icon">
-                    <img src="assets/images/banner/banner-shape.png"/>
+            <header className="header-bottom-4">
+                <img src="assets/images/banner/banner4.jpg"/>
+                <div className="header-bottom-4__bg">
+                    <div className="icon">
+                        <img src="assets/images/banner/banner-shape.png"/>
+                    </div>
                 </div>
-            </div>
-            <div className="container">
-                <h1 className="section-title">Latest <span>Articles</span></h1>
-                <ol className="breadcrumb">
-                    <li className="breadcrumb-item"><a href="/">Home</a></li>
-                    <li className="breadcrumb-item active">Latest articles</li>
-                </ol>
-            </div>
-        </header>
-        
-        <section className="latest-news-4 main-blog">
-            <div className="main-blog__bg">
-                <div className="icon">
-                    <img src="assets/images/png-shapes/form-shape.png"/>
+                <div className="container">
+                    <h1 className="section-title">Latest <span>Articles</span></h1>
+                    <ol className="breadcrumb">
+                        <li className="breadcrumb-item"><a href="/">Home</a></li>
+                        <li className="breadcrumb-item active">Latest articles</li>
+                    </ol>
                 </div>
-                <div className="icon">
-                    <img src="assets/images/png-shapes/specialists__left-bottom-shape.png"/>
-                </div>
-            </div>
-            <div className="container">
-                <a href="blog-details_left.html" className="item">
-                    <div className="img">
-                        <img src="assets/images/jpg/latest-news1.jpg"/>
-                        <span className="date">Now 28. 2019</span>
-                    </div>
-                    <h4 className="title">Improvements in Technology</h4>
-                    <p className="text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-                    </p>
-                    <span className="read-more-2">read more</span>
-                </a>
-                <a href="blog-details_left.html" className="item">
-                    <div className="img">
-                        <img src="assets/images/jpg/latest-news2.jpg"/>
-                        <span className="date">Now 28. 2019</span>
-                    </div>
-                    <h4 className="title">Improvements in Technology</h4>
-                    <p className="text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-                    </p>
-                    <span className="read-more-2">read more</span>
-                </a>
-                <a href="blog-details_left.html" className="item">
-                    <div className="img">
-                        <img src="assets/images/jpg/latest-news3.jpg"/>
-                        <span className="date">Now 28. 2019</span>
-                    </div>
-                    <h4 className="title">Improvements in Technology</h4>
-                    <p className="text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-                    </p>
-                    <span className="read-more-2">read more</span>
-                </a>
-                <a href="blog-details_left.html" class="item">
-                    <div className="img">
-                        <img src="assets/images/jpg/latest-news4.jpg"/>
-                        <span className="date">Now 28. 2019</span>
-                    </div>
-                    <h4 className="title">Improvements in Technology</h4>
-                    <p className="text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-                    </p>
-                    <span className="read-more-2">read more</span>
-                </a>
-                <a href="blog-details_left.html" className="item">
-                    <div className="img">
-                        <img src="assets/images/jpg/latest-news5.jpg"/>
-                        <span className="date">Now 28. 2019</span>
-                    </div>
-                    <h4 className="title">Improvements in Technology</h4>
-                    <p className="text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-                    </p>
-                    <span className="read-more-2">read more</span>
-                </a>
-                <a href="blog-details_left.html" className="item">
-                    <div className="img">
-                        <img src="assets/images/jpg/latest-news6.jpg"/>
-                        <span className="date">Now 28. 2019</span>
-                    </div>
-                    <h4 className="title">Improvements in Technology</h4>
-                    <p className="text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-                    </p>
-                    <span className="read-more-2">read more</span>
-                </a>
-                <a href="blog-details_left.html" className="item">
-                    <div className="img">
-                        <img src="assets/images/jpg/latest-news7.jpg"/>
-                        <span className="date">Now 28. 2019</span>
-                    </div>
-                    <h4 className="title">Improvements in Technology</h4>
-                    <p className="text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-                    </p>
-                    <span className="read-more-2">read more</span>
-                </a>
-                <a href="blog-details_left.html" className="item">
-                    <div className="img">
-                        <img src="assets/images/jpg/latest-news8.jpg"/>
-                        <span className="date">Now 28. 2019</span>
-                    </div>
-                    <h4 className="title">Improvements in Technology</h4>
-                    <p className="text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-                    </p>
-                    <span className="read-more-2">read more</span>
-                </a>
-                <a href="blog-details_left.html" className="item">
-                    <div className="img">
-                        <img src="assets/images/jpg/latest-news9.jpg"/>
-                        <span className="date">Now 28. 2019</span>
-                    </div>
-                    <h4 className="title">Improvements in Technology</h4>
-                    <p className="text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-                    </p>
-                    <span className="read-more-2">read more</span>
-                </a>
-                <ul className="my-pagination">
-                    <li className="disabled">
-                        <span className="fa fa-angle-left"></span>
-                    </li>
-                    <li className="active">
-                        <a href="#">01</a>
-                    </li>
-                    <li className="">
-                        <a href="#">02</a>
-                    </li>
-                    <li className="">
-                        <a href="#">03</a>
-                    </li>
-                    <li className="dotted">
-                        <p>...</p>
-                    </li>
-                    <li className="">
-                        <a href="#">09</a>
-                    </li>
-                    <li className="">
-                        <span className="fa fa-angle-right"></span>
-                    </li>
-                </ul>
-            </div>
-        </section>
+            </header>
 
+            <section className="latest-news-4 main-blog">
+                <div className="main-blog__bg">
+                    <div className="icon">
+                        <img src="assets/images/png-shapes/form-shape.png"/>
+                    </div>
+                    <div className="icon">
+                        <img src="assets/images/png-shapes/specialists__left-bottom-shape.png"/>
+                    </div>
+                </div>
+                <div className="container">
+                    {
+                        articles.map((article, index) =>
+                            <Link to={`${process.env.PUBLIC_URL}/blog/single/${article.id}`} key={index}
+                                  className={"latest-news__item"}>
+                                <div className="inner-block">
+                                    <div className="img">
+                                        <img
+                                            src={`${process.env.REACT_APP_API_URL}/uploads/posts/` + article.fileName}
+                                            alt="product back"/>
+                                        <span className="date">{article.createdAt}</span>
+                                    </div>
+                                    <h4 className="title">{article.titleFr}</h4>
+                                    <span className="read-more-2">read more</span>
+                                </div>
+                            </Link>
+                        )}
+                </div>
+            </section>
         </>
     );
 }
+
+function mapStateToProps(state, props) {
+    return {
+        articles: state.blog.articles ? state.blog.articles : [],
+    }
+}
+
+export default connect(mapStateToProps, {getArticles})(Grid2Cols);
